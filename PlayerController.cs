cce355c0d8dd3f19dc0b5931593f0dc87157efc4
slavkoSenkiv <60449898+slavkoSenkiv@users.noreply.github.com>
2022-01-26 10:12:47 +0200

    upd

--- PlayerController.cs
+++ PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 1000;
     public float gravityModifier = 2;
     public bool onTheGround = true;
+    public bool gameOver = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,14 +20,26 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if (Input.GetKeyDown(KeyCode.Space) && onTheGround)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             onTheGround = false;
         }
     }
+
     private void OnCollisionEnter(Collision collision)
     {
-        onTheGround = true;
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            onTheGround = true;
+        }
+
+        else if (collision.gameObject.CompareTag("obstacle"))
+        {
+            Debug.Log("Game Over");
+            gameOver = true;
+
+        }
     }
 }
